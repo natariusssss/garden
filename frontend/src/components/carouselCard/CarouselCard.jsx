1b149bd5d1ae264ca9d20e29904b890dc9258613
Arbuz-ignor <maksimplov@gmail.com>
2026-04-22 19:43:00 +0300

    New animations

--- frontend/src/components/carouselCard/CarouselCard.jsx
+++ frontend/src/components/carouselCard/CarouselCard.jsx
@@ -1,14 +1,35 @@
-import "./style.css"
+import "./style.css";
 
-const carouselCard = ({selected,id,name,rarity, rarityClass, onClick}) => {
+const carouselCard = ({
+  selected,
+  id,
+  name,
+  rarity,
+  rarityClass,
+  img_small,
+  onClick,
+}) => {
   return (
     <article
-    onClick={onClick}
+      onClick={onClick}
       key={id}
       className={`topic-modal__plant-card${
         selected ? " topic-modal__plant-card--selected" : ""
       }`}
     >
+      <div className="topic-modal__plant-visual">
+        <div className="topic-modal__plant-glow"></div>
+
+        {img_small ? (
+          <img
+            src={img_small}
+            alt={name}
+            className="topic-modal__plant-image"
+          />
+        ) : (
+          <div className="topic-modal__plant-image-placeholder"></div>
+        )}
+      </div>
       <h4 className="topic-modal__plant-name">{name}</h4>
 
       <span
