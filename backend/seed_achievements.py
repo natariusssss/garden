8e5ae9333732ba19c4ffbd685a6fafd82f1cea88
Ксения Евдокимова <ksuuu2711@gmail.com>
2026-05-02 18:32:40 +0300

    update achievements + crud functions

--- backend/seed_achievements.py
+++ backend/seed_achievements.py
@@ -1,3 +1,5 @@
+import code
+
 from main import SessionLocal
 from backend.models import Achievement
 
@@ -36,6 +38,54 @@ def seed_achievements():
             "condition_type": "topics_count",
             "condition_value": 1,
         },
+        {
+            "code": "fifth_topic",
+            "title": "Пятая тема",
+            "description": "Создать пятую тему",
+            "icon_url": None,
+            "condition_type": "topics_count",
+            "condition_value": 5,
+        },
+        {
+            "code": "xp_1000",
+            "title": "1000 XP",
+            "description": "Набрать 1000 XP",
+            "icon_url": None,
+            "condition_type": "total_xp",
+            "condition_value": 1000,
+        },
+        {
+            "code": "first_friend",
+            "title": "Первый друг",
+            "description": "Добавить первого друга",
+            "icon_url": None,
+            "condition_type": "friends_count",
+            "condition_value": 1,
+        },
+        {
+            "code": "fifth_friend",
+            "title": "Пять друзей",
+            "description": "Добавить пять друзей",
+            "icon_url": None,
+            "condition_type": "friends_count",
+            "condition_value": 5,
+        },
+        {
+            "code": "fifth_level",
+            "title": "Пятый уровень",
+            "description": "Достичь пятого уровня",
+            "icon_url": None,
+            "condition_type": "level",
+            "condition_value": 5,
+        },
+        {
+            "code": "seven_days_streak",
+            "title": "7 дней подряд",
+            "description": "Заниматься 7 дней подряд",
+            "icon_url": None,
+            "condition_type": "days_streak",
+            "condition_value": 7
+        }
     ]
 
     try:
